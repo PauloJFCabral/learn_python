8bcb5fec8443ccf9385643aefb522ce8dea60912
Paulo Cabral <paulojorgefreitascabral@gmail.com>
2024-04-28 21:52:19 +0100

    Aula02

---
+++ aula01.py
@@ -0,0 +1,77 @@
+#nomedavariavel = valordavariavel
+
+idade = 36
+nome = "Ana"
+idade = "36"
+
+
+x = str(3)
+y = int(3)
+z = float(3)
+#print(x)
+#print(y)
+#print(z)
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+a = 4
+A = "Ana"
+# print(a)
+# print(A)
+
+#camel case
+#idadeDaPessoa
+#pascal case
+#IdadeDaPessoa
+#snake case
+#idade_da_pessoa
+
+
+### assignar valores as variaveis
+#nome = idade = localidade = "pedro"
+#print(nome)
+#print(idade)
+#print(localidade)
+frutas = ["maça","banana","laranja","pera"]
+x, y, z, p = frutas
+
+#print(x)
+# print(y)
+# print(z)
+# print(p)
+
+
+string = "Python é fixe!"
+#print(string)
+string1 = "Python"
+string2 = "é"
+string3 = "fixe!"
+
+
+#print(string1,string3,string1)
+
+#print(string1+" "+string2+" "+string3)
+#x = 5
+#y = "3"
+#print(x , y)
+
+
+x = "variavel"
+
+print(x)
+def myfunc():
+    global x
+    x = "dentrodafuncao"
+
+myfunc()
+print(x)
+
+
+
+
+
+
+
+
---
+++ aula02.py
@@ -0,0 +1,49 @@
+# resposta = input("Insira um numero: ")
+
+# print("Utilizador inseriu o numero ",resposta)
+
+# print("O numero que o utilizador inseriu mais 5 é ",int(float(resposta)+5))
+
+# strings
+print("Hello")
+print('Hello')
+
+
+print("Ela disse que eu sou 'bonito \"entre\" aspas' 'de novo' ")
+
+a = "O valor da string"
+
+texto = '''Hoje foi um dia productivo.
+fomos a lousada.
+E nãp trouxemos nada de jeito'''
+print(texto)
+
+
+name = "paulo"
+
+print(name[0])
+
+#       0123456789 10111213 14
+txt = "O rato roeu a rolha. O rato comeu o queijo."
+# tamanho_do_txt = len(txt)
+# print("O tamanho do text",tamanho_do_txt)
+
+# print("rato" in txt)
+
+# print(txt[2:6])
+# print(txt[7:])
+# print(txt[:6])
+# print(txt[-4:])
+# print(txt[0])
+
+print(txt.upper())
+print(txt.lower())
+print(txt.strip())
+print(txt.endswith("ro"))
+print(txt.replace("rato","cão",1))
+txt = txt.replace("rato","cão")
+print(txt)
+
+
+
+print("aaaaaaa")
